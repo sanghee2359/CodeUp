e5bb96bc6af499cdecf4f8008a8ffda6f2dca29c
sanghee <wjdtkdgml7352@daum.net>
2022-11-27 17:33:43 +0900

    [수정] 함수로 분리

--- study/1_FindText.java
+++ study/1_FindText.java
@@ -1,14 +1,10 @@
+// 리팩토링 : 함수 생성
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class FindText {
-    public static void main(String[] args) throws IOException {
-        // 입력값 받기
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String st = br.readLine();
-        char c = br.readLine().charAt(0);
-
+    public int findText(char c, String str)  {
         // 문자 c와 비교해서 겹치는 수를 담는 변수
         int count = 0;
 
@@ -16,15 +12,27 @@ public class FindText {
         if(Character.isLowerCase(c)) {
             // 문자열을 소문자로 변환 후 char[] 으로 만든다.
             // for문으로 뿌리기
-            for(char x : st.toLowerCase().toCharArray()) {
+            for(char x : str.toLowerCase().toCharArray()) {
                 // 소문자 x와 소문자 c가 일치하면 개수 추가
                 if(x == c) count++;
             }
-        }else { // 대문자일 경우
-            for(char x : st.toUpperCase().toCharArray())
+        } else { // 대문자일 경우
+            for(char x : str.toUpperCase().toCharArray())
                 // 대문자 x와 대문자 c가 일치하면 개수 추가
                 if(x == c) count++;
         }
-        System.out.println(count);
+        return count;
+    }
+    public static void main(String[] args) throws IOException {
+        // 객체 생성
+        FindText T = new FindText();
+
+        // 입력값 받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String st = br.readLine();
+        char c = br.readLine().charAt(0);
+
+        // 함수 호출
+        System.out.println(T.findText(c, st));
     }
 }
