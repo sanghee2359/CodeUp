664c061dcf9783492062799d24c3adbf1354de0e
sanghee <wjdtkdgml7352@daum.net>
2022-12-03 15:05:09 +0900

    특수 문자 뒤집기 - 제출완료 lt와 rt를 구분해서 if-else if 작성

--- src/String/FlippingCertainCharacters.java
+++ src/String/FlippingCertainCharacters.java
@@ -16,14 +16,17 @@ public class FlippingCertainCharacters {
         int lt = 0, rt = s.length-1;
 
         while(lt < rt){
-            if(((65<=s[lt]&&90>=s[lt])||(97<=s[lt]&&122>=s[lt]))
-                    && ((65<=s[rt]&&90>=s[rt])||(97<=s[rt]&&122>=s[rt]))){
+            if(((65>s[lt])||(90<s[lt])) && ((97>s[lt])||(122<s[lt]))){
+                lt ++;
+            }else if(((65>s[rt])||(90<s[rt])) && ((97>s[rt])||(122<s[rt]))){
+                rt --;
+            }else {
                 char tmp = s[lt];
                 s[lt] = s[rt];
                 s[rt] = tmp;
+                lt++;
+                rt--;
             }
-            lt ++;
-            rt --;
         }
 
         answer = String.valueOf(s);
