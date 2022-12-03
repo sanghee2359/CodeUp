cf44c54ff7091f13195fc6f03cf3e089de360675
sanghee <wjdtkdgml7352@daum.net>
2022-12-03 16:20:52 +0900

    [수정] 회문 문자열 - return 수정

--- src/String/PalindromeString.java
+++ src/String/PalindromeString.java
@@ -8,7 +8,7 @@ public class PalindromeString {
         str = str.toUpperCase(); // 대문자로 모두 변경
         int len = str.length();
         for (int i = 0; i < len/2; i++) { // 홀수건 짝수건 str의 절반길이만큼만 반복실행한다.
-            if(str.charAt(i) != str.charAt(len-i-1)) answer = "NO";
+            if(str.charAt(i) != str.charAt(len-i-1)) return "NO";
         }
         return answer;
     }
