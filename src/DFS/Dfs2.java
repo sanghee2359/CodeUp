349d4aa507476fc2b2d9f7b3e39dc0647edd0c65
sanghee <wjdtkdgml7352@daum.net>
2022-12-16 22:10:36 +0900

    [DFS] 전위순회 (root>left>right)

---
+++ src/DFS/Dfs2.java
@@ -0,0 +1,34 @@
+package DFS;
+class Node{
+    Node lt,rt;
+    int data;
+    public Node(int data) {
+        this.data = data;
+        rt = lt = null;
+    }
+}
+
+public class Dfs2 {
+    Node root;
+    public void DFS(Node root) {
+        if(root == null) return; // root가 가장 말단노드 -> 종료
+        else {
+            System.out.printf(root.data+" ");
+            DFS(root.lt);
+            DFS(root.rt);
+        }
+    }
+    public static void main(String[] args) {
+        Dfs2 tree = new Dfs2();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+        tree.DFS(tree.root); // dfs()에서 깊이우선탐색 코드 구현
+
+
+    }
+}
